7a51bcec7d2f14ad87e09a627c66b7b4bc23cbcc
Muslam <muslam14@gmail.com>
2025-11-04 16:18:06 +0700

    database setting

--- config/database.php
+++ config/database.php
@@ -52,16 +52,14 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8'), // MariaDB compatible
-            'collation' => env('DB_COLLATION', 'utf8_unicode_ci'), // MariaDB compatible
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => false,
+            'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION sql_mode='NO_ENGINE_SUBSTITUTION'",
             ]) : [],
         ],
 
