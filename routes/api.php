3f80a5693d8e391ffca117a620b77e4d9a295acb
Muslam <muslam14@gmail.com>
2025-11-07 17:21:15 +0700

    fix api controller case sensitive

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Api Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register Api routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| is assigned the "api" middleware group. Enjoy building your Api!
 |
 */
 
@@ -20,38 +20,38 @@ Route::middleware('auth.token')->get('/user', function (Request $request) {
 
 // Authentication Routes
 Route::prefix('auth')->group(function () {
-    Route::post('login', [App\Http\Controllers\API\AuthController::class, 'login'])->name('api.auth.login');
-    Route::post('logout', [App\Http\Controllers\API\AuthController::class, 'logout'])->name('api.auth.logout');
-    Route::get('me', [App\Http\Controllers\API\AuthController::class, 'me'])->name('api.auth.me');
+    Route::post('login', [App\Http\Controllers\Api\AuthController::class, 'login'])->name('api.auth.login');
+    Route::post('logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->name('api.auth.logout');
+    Route::get('me', [App\Http\Controllers\Api\AuthController::class, 'me'])->name('api.auth.me');
 });
 
-// Protected API Routes (require authentication)
+// Protected Api Routes (require authentication)
 Route::middleware('auth.token')->group(function () {
-    // API Routes for Transactions
+    // Api Routes for Transactions
     Route::prefix('transactions')->group(function () {
-        Route::get('/', [App\Http\Controllers\API\ApiTransactionController::class, 'index'])->name('api.transactions.index');
-        Route::post('/', [App\Http\Controllers\API\ApiTransactionController::class, 'store'])->name('api.transactions.store');
-        Route::get('/statistics', [App\Http\Controllers\API\ApiTransactionController::class, 'statistics'])->name('api.transactions.statistics');
-        Route::get('/{id}', [App\Http\Controllers\API\ApiTransactionController::class, 'show'])->name('api.transactions.show');
-        Route::put('/{id}', [App\Http\Controllers\API\ApiTransactionController::class, 'update'])->name('api.transactions.update');
-        Route::delete('/{id}', [App\Http\Controllers\API\ApiTransactionController::class, 'destroy'])->name('api.transactions.destroy');
+        Route::get('/', [App\Http\Controllers\Api\ApiTransactionController::class, 'index'])->name('api.transactions.index');
+        Route::post('/', [App\Http\Controllers\Api\ApiTransactionController::class, 'store'])->name('api.transactions.store');
+        Route::get('/statistics', [App\Http\Controllers\Api\ApiTransactionController::class, 'statistics'])->name('api.transactions.statistics');
+        Route::get('/{id}', [App\Http\Controllers\Api\ApiTransactionController::class, 'show'])->name('api.transactions.show');
+        Route::put('/{id}', [App\Http\Controllers\Api\ApiTransactionController::class, 'update'])->name('api.transactions.update');
+        Route::delete('/{id}', [App\Http\Controllers\Api\ApiTransactionController::class, 'destroy'])->name('api.transactions.destroy');
     });
 
-    // API Routes for Accounts
+    // Api Routes for Accounts
     Route::prefix('accounts')->group(function () {
-        Route::get('/', [App\Http\Controllers\API\ApiAccountController::class, 'index'])->name('api.accounts.index');
-        Route::post('/', [App\Http\Controllers\API\ApiAccountController::class, 'store'])->name('api.accounts.store');
-        Route::get('/{id}', [App\Http\Controllers\API\ApiAccountController::class, 'show'])->name('api.accounts.show');
-        Route::put('/{id}', [App\Http\Controllers\API\ApiAccountController::class, 'update'])->name('api.accounts.update');
-        Route::delete('/{id}', [App\Http\Controllers\API\ApiAccountController::class, 'destroy'])->name('api.accounts.destroy');
+        Route::get('/', [App\Http\Controllers\Api\ApiAccountController::class, 'index'])->name('api.accounts.index');
+        Route::post('/', [App\Http\Controllers\Api\ApiAccountController::class, 'store'])->name('api.accounts.store');
+        Route::get('/{id}', [App\Http\Controllers\Api\ApiAccountController::class, 'show'])->name('api.accounts.show');
+        Route::put('/{id}', [App\Http\Controllers\Api\ApiAccountController::class, 'update'])->name('api.accounts.update');
+        Route::delete('/{id}', [App\Http\Controllers\Api\ApiAccountController::class, 'destroy'])->name('api.accounts.destroy');
     });
 
-    // API Routes for Categories
+    // Api Routes for Categories
     Route::prefix('categories')->group(function () {
-        Route::get('/', [App\Http\Controllers\API\ApiCategoryController::class, 'index'])->name('api.categories.index');
-        Route::post('/', [App\Http\Controllers\API\ApiCategoryController::class, 'store'])->name('api.categories.store');
-        Route::get('/{id}', [App\Http\Controllers\API\ApiCategoryController::class, 'show'])->name('api.categories.show');
-        Route::put('/{id}', [App\Http\Controllers\API\ApiCategoryController::class, 'update'])->name('api.categories.update');
-        Route::delete('/{id}', [App\Http\Controllers\API\ApiCategoryController::class, 'destroy'])->name('api.categories.destroy');
+        Route::get('/', [App\Http\Controllers\Api\ApiCategoryController::class, 'index'])->name('api.categories.index');
+        Route::post('/', [App\Http\Controllers\Api\ApiCategoryController::class, 'store'])->name('api.categories.store');
+        Route::get('/{id}', [App\Http\Controllers\Api\ApiCategoryController::class, 'show'])->name('api.categories.show');
+        Route::put('/{id}', [App\Http\Controllers\Api\ApiCategoryController::class, 'update'])->name('api.categories.update');
+        Route::delete('/{id}', [App\Http\Controllers\Api\ApiCategoryController::class, 'destroy'])->name('api.categories.destroy');
     });
 });
